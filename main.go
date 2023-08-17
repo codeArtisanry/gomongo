c686d0735b296bc2af83ca98ba0bb59a50e51f27
Vatsal Chauhan <87218847+codeArtisanry@users.noreply.github.com>
2023-08-17 15:27:10 +0530

    Temp: commit

--- main.go
+++ main.go
@@ -1,15 +1,117 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var client *mongo.Client
+
 func main() {
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	// create a context in order to disconnect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	fmt.Println("ctx is: ", ctx)
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	var result bson.M
+	if err := client.Database("gomongo").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
 	e.Logger.Fatal(e.Start(":1323"))
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "data")
+	})
+	// Root level middleware
+	coll := client.Database("gomongo").Collection("movies")
+	title := "Back to the Future"
+	// var result bson.M
+	// result, err := coll.InsertOne(
+	//   context.TODO(),
+	//   bson.D{
+	//       {Key: "animal", Value: "Dog"},
+	//       {Key: "breed", Value: "Beagle"}
+	//   }
+	// )
+	err = coll.FindOne(context.TODO(), bson.D{{Key: "title", Value: title}}).Decode(&result)
+	fmt.Println(result)
+	if err == mongo.ErrNoDocuments {
+		fmt.Printf("No document was found with the title %s\n", title)
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+	jsonData, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", jsonData)
+
+	// e.Use(middleware.Recover())
+	// e.Use(middleware.CORS())
+
+	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+	// 	AllowOrigins: []string{"*"},
+	// 	AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+	// 	AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	// }))
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	// create connection options
+	clientOptions := options.Client().ApplyURI(uri)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+		return nil, err
+	}
+
+	log.Println("Connected to mongo!")
+
+	return c, nil
 }
